b9dd89f328dd220de44710d20cb0f753dcea74bc
Masiu <ciszekmarcell@gmail.com>
2019-08-21 20:10:34 +0200

    add

--- server.js
+++ server.js
@@ -11,10 +11,17 @@ connectDB();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'client/build')));
-
 app.use('/api/users', require('./routes/user'));
 app.use('/api/tasks', require('./routes/task'));
+
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.send(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () =>
   console.log(`Server is running on localhost on port ${port}`)
