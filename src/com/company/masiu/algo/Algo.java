a713b549b3466a86796782e8501c6c98e27a68fa
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-04-21 10:02:54 +0200

    feat: tandemBicycle

--- src/com/company/masiu/algo/Algo.java
+++ src/com/company/masiu/algo/Algo.java
@@ -1,12 +1,9 @@
 package com.company.masiu.algo;
 
-import java.sql.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class Algo {
 
@@ -149,7 +146,7 @@ public class Algo {
       for (int j = i + 1; j < array.length; j++) {
         int toCompareWith = array[j];
         if (current == toCompareWith) {
-            minIndex = Math.min(minIndex,j);
+          minIndex = Math.min(minIndex, j);
         }
       }
     }
@@ -167,5 +164,37 @@ public class Algo {
       hasSeenNumber.put(n, true);
     }
     return -1;
+  }
+
+  public int tandemBicycle(int[] reds, int[] blues, boolean fastest) {
+
+    Arrays.sort(reds);
+    Arrays.sort(blues);
+
+    if (!fastest) {
+      this.reverse(reds);
+    }
+
+    int result = 0;
+    for (int i = 0; i < reds.length; i++) {
+      int rider1 = reds[i];
+      int rider2 = blues[blues.length - i - 1];
+      result += Math.max(rider1, rider2);
+    }
+
+    return result;
+  }
+
+  private void reverse(int[] xs) {
+    int start = 0;
+    int end = xs.length - 1;
+
+    while (start < end) {
+      int temp = xs[start];
+      xs[start] = xs[end];
+      xs[end] = temp;
+      start++;
+      end--;
+    }
   }
 }
--- src/com/company/masiu/algo/AlgoDemo.java
+++ src/com/company/masiu/algo/AlgoDemo.java
@@ -7,17 +7,19 @@ public class AlgoDemo {
   public static void main(String[] args) {
     Algo algo = new Algo();
 
-    int[] nums = {1, 2, 4, 5, 6, 7, 8, 9};
-    int[] res = algo.sortedSquaredArray(nums);
-    printList(res);
+    int[] reds = {5, 5, 3, 9, 2};
+    int[] blues = {3, 6, 7, 2, 1};
+    boolean fastes = true;
 
+    int res = algo.tandemBicycle(reds, blues, fastes);
+    print(res);
   }
 
   private static <T> void print(T value) {
     System.out.println(value);
   }
 
-  private static  void printList(int[] value) {
+  private static void printList(int[] value) {
     System.out.println(Arrays.toString(value));
   }
 }
