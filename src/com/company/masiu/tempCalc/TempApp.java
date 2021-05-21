8706db863b7a14947a5f828ba6187cee27b45750
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-05-21 16:01:33 +0200

    java temp calculator

--- src/com/company/masiu/tempCalc/TempApp.java
+++ src/com/company/masiu/tempCalc/TempApp.java
@@ -1,13 +1,31 @@
 package com.company.masiu.tempCalc;
 
+import java.util.Scanner;
+
 public class TempApp {
 
   public static void main(String[] args) {
-    float x = fromCelsiusToFahrenheit(20);
-    float x2 = fromFahrenheitToCelsius(20);
+    Scanner scanner = new Scanner(System.in);
 
-    String formattedString = String.format("%.02f", x2);
-    System.out.println(formattedString);
+    print("enter your degree");
+    float degree = Float.parseFloat(scanner.nextLine());
+    print(degree);
+
+    print("do you want to convert it Celsius or Fahrenheit? Enter C for Celsius or F for Fahrenheit");
+    String measureType = scanner.nextLine();
+
+    if (measureType.toLowerCase().equals("c")) {
+      float finalResult = fromCelsiusToFahrenheit(degree);
+      String formattedResult = String.format("Celsius to Fahrenheit is =  %.02f F", finalResult);
+      print(formattedResult);
+    } else if (measureType.toLowerCase().equals("f")) {
+      float finalResult = fromFahrenheitToCelsius(degree);
+      String formattedResult = String.format("fahrenheit to Celsius is =  %.02f C", finalResult);
+      print(formattedResult);
+    } else {
+      String notValidUnit = String.format("could not parse %s", measureType);
+      print(notValidUnit);
+    }
   }
 
   private static float fromCelsiusToFahrenheit(float celsius) {
@@ -16,5 +34,9 @@ public class TempApp {
 
   private static float fromFahrenheitToCelsius(float fahrenheit) {
     return (fahrenheit - 32) * 5 / 9;
+  }
+
+  private static <T> void print(T x) {
+    System.out.println(x);
   }
 }
