8ff5781c29c39eaec73f8ecc73f60fb1de095b41
Marcell Ciszek <marcell.ciszek@etraveligroup.com>
2021-07-20 17:57:25 +0200

    JAVA Queue

--- src/com/company/masiu/dataStructures/App.java
+++ src/com/company/masiu/dataStructures/App.java
@@ -5,6 +5,12 @@ import com.company.masiu.Print;
 public class App {
 
   public static void main(String[] args) {
+    Queue<Integer> q = new Queue<>();
+    q.enqueue(2);
+    q.enqueue(33);
+    q.enqueue(45);
+    q.deque();
+    q.print();
 
   }
 }
---
+++ src/com/company/masiu/dataStructures/Queue.java
@@ -0,0 +1,74 @@
+package com.company.masiu.dataStructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Node<T> {
+
+  public T data;
+  Node<T> next;
+
+  public Node(T data) {
+    this.data = data;
+    this.next = null;
+  }
+}
+
+public class Queue<T> {
+
+  public Node<T> first;
+  public Node<T> last;
+  private int size;
+
+  public Queue() {
+    this.first = null;
+    this.last = null;
+    this.size = 0;
+  }
+
+  public Node<T> enqueue(T key) {
+    Node<T> newNode = new Node<>(key);
+    if (this.first == null) {
+      this.first = newNode;
+      this.last = newNode;
+      this.size++;
+      return newNode;
+    }
+    this.last.next = newNode;
+    this.last = newNode;
+
+    return newNode;
+  }
+
+  public boolean deque() {
+    if (this.first == null) return false;
+    if (this.first == this.last) {
+      this.last = null;
+      this.size--;
+      return true;
+    }
+    this.first = this.first.next;
+    return true;
+  }
+
+  public Node<T> peek() {
+
+    return first;
+  }
+
+  public int getSize() {
+    return size;
+  }
+
+  public void print() {
+    List<T> xs = new ArrayList<>();
+    Node<T> current = this.first;
+    while (current != null) {
+      xs.add(current.data);
+      current = current.next;
+    }
+    for (T x : xs) {
+      System.out.println(x);
+    }
+  }
+}
