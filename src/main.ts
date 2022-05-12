51ec17c2515c91771ddf00b3852a57546f2e6336
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-05-12 09:06:57 +0200

    Set up html Routes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
-import './style.css'
+import "./style.css"
+import "./home.css"
 
-const app = document.querySelector<HTMLDivElement>('#app')!
+const navItems = [
+  {name: "DOM", path: "./src/pages/dom.html"},
+  {name: "Naviagtor", path: "./src/pages/navigator.html"},
+  {name: "Copy", path: "./src/pages/copy.html"},
+  {name: "Permission API", path: "./src/pages/permission-api.html"},
+]
+
+const app = document.querySelector<HTMLDivElement>("#app")!
 
 app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+<div class="box">
+  <ul>    
+    ${navItems.map(item => `<li> <a href=${item.path}>${item.name}</a>   </li>`).join("")}
+  </ul>
+</div>
 `
