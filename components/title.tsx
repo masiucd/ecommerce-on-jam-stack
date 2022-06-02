ad54493774b6a3e927a3e4ea052928e2fec9c4c6
Marcell Ciszek <ciszekmarcell@gmail.com>
2022-06-02 21:00:19 +0200

    Card image

--- components/title.tsx
+++ components/title.tsx
@@ -1,7 +1,16 @@
 import React from "react"
 
-const Title = () => {
-  return <div>Title</div>
+interface Props {
+  title?: string
+  className?: string
+  children?: React.ReactNode
+}
+const Title = ({title, className, children}: Props) => {
+  return children ? (
+    <aside className={`text-2xl md:text-4xl  ${className}`}>{children}</aside>
+  ) : (
+    <h1 className={className}>{title}</h1>
+  )
 }
 
 export default Title
